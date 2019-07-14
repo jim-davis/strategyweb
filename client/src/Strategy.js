dc7a87501a7c401881ee0f3aa938e59136cff7e8
Jim Davis <jrd3@alum.mit.edu>
2019-07-14 12:38:46 -0400

    Move Header to a separate component. Add tabs

--- client/src/Strategy.js
+++ client/src/Strategy.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './Strategy.css';
+import Header from './Header.js';
 import Footer from './Footer.js';
 import Matches from './Matches.js';
 import Teams from './Teams.js';
 import PickList from './PickList.js';
 import axios from 'axios';
 
-import arctos_logo from './arctos_logo.png';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
+
+
 
 const API = "http://localhost:3001";
-
-function api(method) {
-    return API + "/api/" + method;
-}
-
 
 class Strategy extends React.Component {
     constructor(props) {
@@ -29,6 +28,7 @@ class Strategy extends React.Component {
         this.getEvents();
 		if (this.state.event_code) {
 			this.getTeams(this.state.event_code);
+            this.getMatches(this.state.event_code);
 		}
     }
 
@@ -61,60 +61,36 @@ class Strategy extends React.Component {
 
     render() {
         return (
-                <div className="Strategy">
-                  <header>
-                    <EventBar 
-			            event_code={this.state.event_code} 
-                        events={this.state.events}
-                        onEventChange={this.eventSelected}/>
-                    <div className="titleBar">
-                      <img src={arctos_logo}  alt="Artos" height="150" />
-                      <h1>Arctos Strategy App Prototype</h1>
-                    </div>
-  
-                </header>
-                <div className='app'>
-                  <PickList/>
-                  <Matches event_code={this.state.event_code} matches={this.state.matches}/>
-                  <Teams event_code={this.state.event_code} teams={this.state.teams}/>
-                </div>
-                <Footer/>
-                </div>
+        <div className="Strategy">
+          <Header event_code={this.state.event_code} events={this.state.events} eventSelected={this.eventSelected}/>
+          <div className='app'>
+            <Tabs>
+              <TabList>
+                <Tab>Matches</Tab>
+                <Tab>Teams</Tab>
+                <Tab>Pick List</Tab>
+              </TabList>
+              <TabPanel>
+                <Matches event_code={this.state.event_code} matches={this.state.matches}/>
+              </TabPanel>
+              <TabPanel>
+                <Teams event_code={this.state.event_code} teams={this.state.teams}/>
+              </TabPanel>
+              <TabPanel>
+                <PickList/>
+              </TabPanel>
+            </Tabs>
+          </div>
+          <Footer/>
+        </div>
         );
     }
 }
 
-// Shows the currently selected Event, and a control to select a different one.
-// Probably while we are at an event that control should be locked down.
-class EventBar extends React.Component {
-	constructor(props) {
-		super(props);
-		this.onEventSelect = this.onEventSelect.bind(this);
-	}
-
-	// When user chooses an event from the event dropdown
-	onEventSelect(e) {
-		// tell our parent that something changed.
-		this.props.onEventChange(e.target.value);
-	}
-
-	render() {
-		return (
-        <div className='eventBar'>
-            <h3>Event {this.props.event_code}</h3>
-            <select onChange={this.onEventSelect}>
-				<option key='none' value=''>what event are you at?</option>
-            {this.props.events.map(evt =>
-			<option 
-               name={evt.event_code} 
-               key={evt.event_code}
-			   >
-              {evt.event_code}
-             </option>)}
-            </select>
-        </div>
-    );
-	}
+// return the URL to invoke the API method
+function api(method) {
+    return API + "/api/" + method;
 }
+
 
 export default Strategy;
