335a749b8fd093a6738ec85c5193bb8b927dae2f
Jim Davis <jrd3@alum.mit.edu>
2019-07-13 23:48:56 -0400

    When you pick an event, the set of teams at that event now updates

--- client/src/Strategy.js
+++ client/src/Strategy.js
@@ -17,24 +17,46 @@ function api(method) {
 class Strategy extends React.Component {
     constructor(props) {
         super(props);
+		this.eventSelected = this.eventSelected.bind(this);
         this.state={events: [],
+					teams: [],
                    event_code: null};
     }
 
     componentDidMount() {
         this.getEvents();
+		if (this.state.event_code) {
+			this.getTeams(this.state.event_code);
+		}
     }
 
+	// a new Event has been selected (in the event bar)
+	eventSelected(event_code) {
+		console.log("An event has been chosen: " + event_code);
+		this.setState({event_code: event_code});
+		this.getTeams(event_code);
+	}
+
+	// get the list of all events from the server
     getEvents() {
         axios.get(api("getEvents"))
 			.then(response => this.setState({ events: response.data.data}));
     }
 
+	getTeams(event_code) {
+		console.log("getting teams for " + event_code);
+		axios.get("http://localhost:3001/api/getTeams?event_code=" + event_code)
+			.then((response) => this.setState({ teams: response.data.data }));
+	}
+
     render() {
         return (
                 <div className="Strategy">
                   <header>
-                    <EventBar event_code={this.state.event_code} events={this.state.events}/>
+                    <EventBar 
+			            event_code={this.state.event_code} 
+                        events={this.state.events}
+                        onEventChange={this.eventSelected}/>
                     <div className="titleBar">
                       <img src={arctos_logo}  alt="Artos" height="150" />
                       <h1>Arctos Strategy App Prototype</h1>
@@ -43,7 +65,7 @@ class Strategy extends React.Component {
                 </header>
                 <div className='app'>
                   <PickList/>
-                  <Teams event_code={this.state.event_code}/>
+                  <Teams event_code={this.state.event_code} teams={this.state.teams}/>
                 </div>
                 <Footer/>
                 </div>
@@ -51,25 +73,27 @@ class Strategy extends React.Component {
     }
 }
 
+// Shows the currently selected Event, and a control to select a different one.
+// Probably while we are at an event that control should be locked down.
 class EventBar extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state={event_code: props.event_code,
-					events: props.events};
+		this.onEventSelect = this.onEventSelect.bind(this);
 	}
 
-	// Called before render.
-	// Our props have changed, but React does not re-render unless we explicitly ask
-	static getDerivedStateFromProps(props, state) {
-		return {events: props.events, event_code: props.event_code};
+	// When user chooses an event from the event dropdown
+	onEventSelect(e) {
+		// tell our parent that something changed.
+		this.props.onEventChange(e.target.value);
 	}
 
 	render() {
 		return (
         <div className='eventBar'>
-            <h2>Event {this.state.event_code}</h2>
-            <select onChange={() => console.log("chg")}>
-            {this.state.events.map(evt =>
+            <h3>Event {this.props.event_code}</h3>
+            <select onChange={this.onEventSelect}>
+				<option key='none' value=''>what event are you at?</option>
+            {this.props.events.map(evt =>
 			<option 
                name={evt.event_code} 
                key={evt.event_code}
--- client/src/Teams.js
+++ client/src/Teams.js
@@ -1,33 +1,21 @@
 import React from 'react';
 
-import axios from 'axios';
-
 class Teams extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {event_code: props.event_code, teams: []};
-	}
-
-	componentDidMount() {
-		axios.get("http://localhost:3001/api/getTeams?event_code=" + this.state.event_code)
-			.then((response) => this.setState({ teams: response.data.data }));
-	}
-
-	static getDerivedStateFromProps(props, state) {
-		return {event_code: props.event_code};
 	}
 
 	render() {
 		return(
 			<div>
 				<h2>Teams</h2>
-				{this.state.event_code ? (
+				{this.props.event_code ? (
 				<table>
 				<thead>
 				<tr><th>Number</th><th>Name</th></tr>
 				</thead>
 				<tbody>
-				{this.state.teams.map(team => <tr key={team.team_number}><td>{team.team_number}</td><td>{team.name}</td></tr>)}
+				{this.props.teams.map(team => <tr key={team.team_number}><td>{team.team_number}</td><td>{team.name}</td></tr>)}
 				</tbody>
 				</table>) : <span>Select an event</span>}
 				</div>
